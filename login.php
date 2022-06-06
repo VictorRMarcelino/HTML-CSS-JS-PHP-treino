ffe179fa1ceddda1d067868bff043a6d3bd1a7b1
VictorRMarcelino <victorramosmarcelino@hotmail.com>
2022-06-05 21:27:55 -0300

    log

--- login.php
+++ login.php
@@ -7,17 +7,24 @@ function executaLogin(){
     $usuario = $_POST["usuario"];
     $senha = $_POST["password"];
 
-    $sql = "select * from public.usuario where usuario = $usuario AND senha = $senha;";
+    $sql = "select * from public.usuario where nome = '$usuario' AND senha = '$senha'";
 
-    $oQuery->executaQuery($sql);
+    $sql_query = $oQuery->executaQuery($sql);
 
-    $quantidade = $oQuery->num_rows;
-
+    $quantidade = $sql_query->num_rows;
 
     if($quantidade == 1){
         require_once("PaginaInicial.html");
     } else {
         echo 'Parametros invalidos!';
     }
+}
+if(isset($_POST["acao"])){
+    $acao = $_POST["acao"];
+    if($acao === "EXECUTA_LOGIN"){
+        executaLogin();
+    }
+} else {
+    echo 'Parametros invalidos!';
 }
 ?>
