adef3fb9f33dff1632f78fa584bbfc1fd85aa429
VictorRMarcelino <victorramosmarcelino@hotmail.com>
2022-06-16 17:11:38 -0300

    ex1

---
+++ js/ex1.js
@@ -0,0 +1,9 @@
+function limparCampos(){
+    var nome = document.getElementById("nome");
+    var email = document.getElementById("email");
+    var obs = document.getElementById("observacoes");
+
+    nome.innerHTML = "";
+    email.innerHTML = "";
+    obs.innerHTML = "";
+}
